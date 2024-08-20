4d5f3b4ab57128b8a86956096ad8d10a2f3942c0
durellwilsondetroit@gmail.com <durellwilsondetroit@gmail.com>
2024-08-19 22:20:58 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+let firstName = "Durell";
+
+let lastName = "Wilson";
+
+let fullName = `${firstName} ${lastName}`;
+console.log(fullName);
+
+let population = 673104;
+console.log(population);
+
+let multiplyPopulationByThree = population *3;
+console.log(multiplyPopulationByThree);
+
+let javaScriptIsCool = true;
+
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", 
+"violet"];
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
